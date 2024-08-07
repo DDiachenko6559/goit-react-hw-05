9f128f46312406f8a9ab53930be970ddfb325193
DDiachenko6559 <153743209+DDiachenko6559@users.noreply.github.com>
2024-08-07 21:38:09 +0300

    +

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,33 @@
+import s from "./HomePage.module.css";
+
+import { useEffect, useState } from "react";
+
+import MovieList from "../../components/MovieList/MovieList";
+import searchMovies from "../../services/api";
+
+const HomePage = () => {
+  const [trendingMovies, setTrendingMovies] = useState([]);
+  const BASE_URL = "https://api.themoviedb.org/3/trending/movie/day";
+
+  useEffect(() => {
+    const searchTrendingMoviesToday = async () => {
+      try {
+        const response = await searchMovies(BASE_URL);
+
+        setTrendingMovies(response);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    searchTrendingMoviesToday();
+  }, []);
+
+  return (
+    <div className={s.wrapper}>
+      <h1>Trending today</h1>
+      <MovieList dataMovies={trendingMovies} />
+    </div>
+  );
+};
+
+export default HomePage;
---
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -0,0 +1,7 @@
+import s from "./NotFoundPage.module.css";
+
+const NotFoundPage = () => {
+  return <h2 className={s.title}>Oops... Page did not found!</h2>;
+};
+
+export default NotFoundPage;
